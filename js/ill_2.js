39d88cf74d9ebbaadd433bf8e803e9d076c527e7
Gaurav Roy <gaurav.roy@bigdecisions.com>
2015-04-08 22:26:01 +0530

    Testing some SVG

--- js/ill_2.js
+++ js/ill_2.js
@@ -11,4 +11,14 @@ $(document).ready(function(){
 			return [ +d.number , +d.frequency ]; });
 		console.log(dataset);
 	});
+
+	d3.select("body").append("svg").attr("width", 200).attr("height", 200).append("circle").attr("cx", 50).attr("cy", 50).attr("r", 40).style("fill", "purple");
+
+	/*
+	
+	var arc=d3.svg.arc()
+		.innerRadius(50).outerRadius(200)
+		.startAngle(0).endAngle(Math.PI*0.26);
+*/
+
 });
